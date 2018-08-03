8c2c82db58528790e06fb8ee66242c6d01acad0c
dodevs <douglass.sousa@outlook.com.br>
2018-08-03 15:40:17 -0300

    Added method to get restaurant details and HTML template

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { RestaurantsService } from '../restaurants/restaurants.service';
+import { Restaurant } from "app/restaurants/restaurant/restaurant.module";
+
 @Component({
   selector: 'mt-restaurant-detail',
   templateUrl: './restaurant-detail.component.html'
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  restaurant: Restaurant;
+  restaurantId: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private restaurantService: RestaurantsService
+  ) { }
 
   ngOnInit() {
+    this.restaurantId = this.route.snapshot.params['id'];
 
+    this.restaurantService.restaurantById(this.restaurantId)
+      .subscribe(restaurant => this.restaurant = restaurant)
   }
 
 }
