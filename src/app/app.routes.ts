4969fd3f6090d41e8e984f13c28e739d50c08420
dodevs <douglass.sousa@outlook.com.br>
2018-08-03 16:35:41 -0300

    Added children routes to menu and reviws access

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from  './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { MenuComponent } from "app/restaurant-detail/menu/menu.component";
 
 const ROUTES: Routes = [
   {
@@ -21,7 +24,12 @@ const ROUTES: Routes = [
   },
   {
     path: 'restaurants/:id',
-    component: RestaurantDetailComponent
+    component: RestaurantDetailComponent,
+    children: [
+      {path: '', redirectTo: 'menu', pathMatch: 'full'},
+      {path: 'menu', component: MenuComponent},
+      {path: 'reviews', component: ReviewsComponent}
+    ]
   }
 ]
 
