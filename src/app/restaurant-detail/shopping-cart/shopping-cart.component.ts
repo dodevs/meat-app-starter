713d9e1c0ce6d4aed0eb24fded286b47cf742bb3
dodevs <douglass.sousa@outlook.com.br>
2018-08-06 16:34:49 -0300

    Components

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { ShoppingCartService } from './shopping-cart.service';
 
+import { CartItem } from './cart-item.model';
+
 @Component({
   selector: 'mt-shopping-cart',
   templateUrl: './shopping-cart.component.html'
@@ -16,6 +18,22 @@ export class ShoppingCartComponent implements OnInit {
 
   items(): any[] {
     return this.shoppingCartService.items;
+  }
+
+  total(): number {
+    return this.shoppingCartService.total();
+  }
+
+  clear() {
+    this.shoppingCartService.clear();
+  }
+
+  removeItem(item: any) {
+    this.shoppingCartService.removeItem(item);
+  }
+
+  addItem(item: any) {
+    this.shoppingCartService.addItem(item);
   }
 
 }
