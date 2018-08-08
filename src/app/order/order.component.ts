120f1af7effa5c450f77cbb0e21c485e411b4062
dodevs <douglass.sousa@outlook.com.br>
2018-08-08 18:09:51 -0300

    Properties and methods

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RadioOption } from "app/shared/radio/radio-option.module";
+import { OrderService } from './order.service';
+import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 
 @Component({
   selector: 'mt-order',
@@ -7,15 +9,39 @@ import { RadioOption } from "app/shared/radio/radio-option.module";
 })
 export class OrderComponent implements OnInit {
 
+  delivery: number = 8;
+
   paymentOptions: RadioOption[] = [
     {label:'Dinheiro', value:'MON'},
     {label:'Cartão de crédito', value: 'DEB'},
     {label:'Cartão Refeição', value: 'REF'}
   ]
 
-  constructor() { }
+  constructor(
+    private orderService: OrderService
+  ) { }
 
   ngOnInit() {
+  }
+
+  itemsValue(): number {
+    return this.orderService.itemsValue();
+  }
+
+  cartItems(): CartItem[]{
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem){
+    return this.orderService.increaseQty(item);
+  }
+
+  decreaseQty(item: CartItem){
+    return this.orderService.decreaseQty(item);
+  }
+
+  remove(item: CartItem){
+    return this.orderService.remove(item);
   }
 
 }
