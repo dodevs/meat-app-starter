59e89f1aa0be24649369717390b7f48e7ab154a3
dodevs <douglass.sousa@outlook.com.br>
2018-08-08 18:08:54 -0300

    Added OrderService and delivery component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,12 +17,15 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
-import { RestaurantsService } from './restaurants/restaurants.service';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
 import { InputComponent } from './shared/input/input.component';
 import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component';
+
+import { RestaurantsService } from './restaurants/restaurants.service';
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
+import { OrderService } from './order/order.service';
+import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 
 
 @NgModule({
@@ -41,7 +44,8 @@ import { OrderItemsComponent } from './order/order-items/order-items.component';
     OrderComponent,
     InputComponent,
     RadioComponent,
-    OrderItemsComponent
+    OrderItemsComponent,
+    DeliveryCostsComponent
   ],
   imports: [
     BrowserModule,
@@ -55,7 +59,8 @@ import { OrderItemsComponent } from './order/order-items/order-items.component';
     {
       provide: LOCALE_ID,
       useValue: 'pt-BR'
-    }
+    },
+    OrderService
   ],
   bootstrap: [AppComponent]
 })
