bef48026e1620a392f99e40b455a2e93d441f1d3
dodevs <douglass.sousa@outlook.com.br>
2018-08-08 18:57:13 -0300

    Update Project

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from "../../../node_modules/@angular/core";
+
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from "rxjs/Observable";
+import 'rxjs/operator/map';
+
 import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
 import { CartItem } from "../restaurant-detail/shopping-cart/cart-item.model";
+import { Order, OrderItem } from "./order.model";
+
+import { MEAT_API } from '../app.api'
 
 @Injectable()
 export class OrderService {
     constructor(
-        private cartService: ShoppingCartService
+        private cartService: ShoppingCartService,
+        private http: Http
     ){}
 
     cartItems(): CartItem[]{
@@ -26,5 +35,21 @@ export class OrderService {
 
     itemsValue(): number{
         return this.cartService.total()
+    }
+
+    checkOrder(order: Order): Observable<string>{
+
+        const headers = new Headers();
+        headers.append('COntent-Type', 'application/json');
+        // JSON.stringify faz uma representação textual do objeto
+        return this.http.post(
+            `${MEAT_API}/orders`, 
+            JSON.stringify(order),
+            new RequestOptions({'headers':headers})
+        ).map( response => response.json() )
+    }
+
+    clear(){
+        this.cartService.clear();
     }
 }
