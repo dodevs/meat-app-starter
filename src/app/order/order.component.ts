be08dcc5d5f8f2915fa0c6366b2db18df52c0fc9
dodevs <douglass.sousa@outlook.com.br>
2018-08-09 18:55:55 -0300

    Added validators

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
 
 import { RadioOption } from "app/shared/radio/radio-option.module";
 import { OrderService } from './order.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
+import { validateConfig } from '../../../node_modules/@angular/router/src/config';
+import { ValueTransformer } from '../../../node_modules/@angular/compiler/src/util';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  emailPattern: RegExp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+  numberPattern: RegExp = /^[0-9]*$/
 
   orderForm: FormGroup
 
@@ -32,13 +38,13 @@ export class OrderComponent implements OnInit {
 
   ngOnInit() {
     this.orderForm = this.fb.group({
-      name: this.fb.control(''), // Valor do input
-      email: this.fb.control(''),
-      emailConfirmation: this.fb.control(''),
-      address: this.fb.control(''),
-      number: this.fb.control(''),
+      name: this.fb.control('', [Validators.required, Validators.minLength(5)]), // Valor do input
+      email: this.fb.control('',[Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.fb.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.fb.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.fb.control('',[Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.fb.control(''),
-      paymentOption: this.fb.control('')
+      paymentOption: this.fb.control('',[Validators.required])
     })
   }
 
