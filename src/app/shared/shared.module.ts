70f2b1285d7c8f7a46fa8d4e2a5b8dc7c0b8a93e
dodevs <douglass.sousa@outlook.com.br>
2018-08-15 17:14:25 -0300

    Added Snackbar componente to SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,15 +5,20 @@ import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
 import { RatingComponent } from "./rating/rating.component";
+import { SnackbarComponent } from './messages/snackbar/snackbar.component';
+
 import { RestaurantsService } from "../restaurants/restaurants.service";
 import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
 import { OrderService } from "../order/order.service";
 
 @NgModule({
-    declarations: [InputComponent, RadioComponent, RatingComponent],
+    declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
     imports: [CommonModule, FormsModule, ReactiveFormsModule],
-    exports: [InputComponent, RadioComponent, RatingComponent, 
-        CommonModule, FormsModule, ReactiveFormsModule]
+    exports: [
+        InputComponent, RadioComponent,
+         RatingComponent, SnackbarComponent,
+        CommonModule, FormsModule, ReactiveFormsModule
+    ]
 })
 export class SharedModule {
     static forRoot(): ModuleWithProviders {
