a1f4255513fbb62cad3dae5f727af534b9e7453f
dodevs <douglass.sousa@outlook.com.br>
2018-08-15 18:27:48 -0300

    notificationService injected

--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -1,7 +1,17 @@
+import { Injectable } from "@angular/core";
+
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
 import { MenuItem } from '../menu-item/menu-item.model';
 
+import { NotificationService } from "../../shared/messages/notification.service";
+
+@Injectable()
 export class ShoppingCartService {
+
+  constructor(
+    private notificationService: NotificationService
+  ){}
+
   items: CartItem[] = [];
 
   clear(){
@@ -17,6 +27,7 @@ export class ShoppingCartService {
     }else{
       this.items.push(new CartItem(item));
     }
+    this.notificationService.notify(`Você adicionou o item ${item.name}`)
   }
 
   increaseQty(item: CartItem){
@@ -32,6 +43,7 @@ export class ShoppingCartService {
 
   removeItem(item: CartItem){
     this.items.splice(this.items.indexOf(item), 1);
+    this.notificationService.notify(`Você removeu o item ${item.menuItem.name}`)
   }
 
   total(): number {
