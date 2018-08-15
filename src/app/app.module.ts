d036dfb4678a23ce4112f868e6eca8a6b53ac975
dodevs <douglass.sousa@outlook.com.br>
2018-08-15 11:21:03 -0300

    Removed imports of services and added core module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
+/* Modulos */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-
 import { AppRoutingModule } from './app.routes';
+/* Modulos */
 
+/* Componentes */
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -15,11 +17,10 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+/* Components */
 
-import { RestaurantsService } from './restaurants/restaurants.service';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderService } from './order/order.service';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 
 
@@ -41,16 +42,14 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     HttpModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
   providers: [
-    RestaurantsService,
-    ShoppingCartService,
     {
       provide: LOCALE_ID,
       useValue: 'pt-BR'
-    },
-    OrderService
+    }
   ],
   bootstrap: [AppComponent]
 })
