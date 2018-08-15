cce1a589ae3e2e65d7e7ed39a64388bc7a7a116a
dodevs <douglass.sousa@outlook.com.br>
2018-08-15 15:56:06 -0300

    New function to use module with providers

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from "../../../node_modules/@angular/core";
+import { NgModule, ModuleWithProviders } from "../../../node_modules/@angular/core";
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
 import { RatingComponent } from "./rating/rating.component";
+import { RestaurantsService } from "../restaurants/restaurants.service";
+import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
+import { OrderService } from "../order/order.service";
 
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent],
@@ -13,5 +16,10 @@ import { RatingComponent } from "./rating/rating.component";
         CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class SharedModule {
-
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SharedModule, // Todas essas configurações para serem usadas
+            providers: [RestaurantsService, ShoppingCartService, OrderService]
+        }
+    }
 }
